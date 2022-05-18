3084ea99f384152045783de24aa3bb81a6622095
nonfungiblesolana <103703809+nonfungiblesolana@users.noreply.github.com>
2022-05-18 21:29:36 +0200

    Update main.chunk.js

--- static/js/main.chunk.js
+++ static/js/main.chunk.js
@@ -541,7 +541,7 @@
                                 disabled: j || R || !u,
                                 onClick: ae,
                                 variant: "containedSecondary",
-                                children: j ? "SOLD OUT" : u ? R ? Object(K.jsx)(O.a, {}) : "MINT (0.03 ◎)" : Object(K.jsx)(g.a, {
+                                children: j ? "SOLD OUT" : u ? R ? Object(K.jsx)(O.a, {}) : "MINT (0.10 ◎)" : Object(K.jsx)(g.a, {
                                     date: Q,
                                     onMount: function(e) {
                                         return e.completed && s(!0)
